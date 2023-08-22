a41f2ca24a2c9c1f6b16ca4742822d314629dc80
tobytrixx <tobytrixx@gmail.com>
2023-08-22 18:55:35 +0100

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
+import { Routes } from 'react-router-dom';
 import SampleMovies from './SampleMovies';
 import Filter from './components/Filter';
 import MovieList from './components/MovieList';
+import EOT from './components/EoT';
 
 function App() {
   return (
---
+++ src/components/EoT.jsx
@@ -0,0 +1,12 @@
+
+
+const EOT = () => {
+
+    return(
+        <div>
+            <h2>Edge of Tomorrow</h2>
+        </div>
+    )
+};
+
+export default EOT;
